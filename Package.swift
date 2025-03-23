724aee953d3c61c5787a8ab15bffab19755d141d
Claudius Ma <heiclaudius@gmail.com>
2025-03-23 21:27:15 +0800

    Revert "refactor: remove unused DayDreamerDesignSystem.swift file"
    This reverts commit 72f81c471c74f8644b7ef5b150b3eeeb3e0d060c.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .target(
             name: "DaydreamerDesignSystem",
             resources: [
-                .process("Colors.xcassets")  
+                .process("Colors.xcassets")
             ]),
 
     ]
