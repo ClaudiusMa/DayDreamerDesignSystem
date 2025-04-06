4fa88d6d66824710fa9428a317bc3a6cb1d69c92
Claudius Ma <heiclaudius@gmail.com>
2025-04-06 15:03:43 +0800

    refactor(design-system): move Radius and Spacing enums to separate files

---
+++ Sources/DayDreamerDesignSystem/Radius.swift
@@ -0,0 +1,14 @@
+//
+//  Radius.swift
+//  DaydreamerDesignSystem
+//
+//  Created by Claudius Ma on 4/6/25.
+//
+import SwiftUI
+// Radius
+public enum Radius {
+  public static let r300: CGFloat = 12
+  public static  let r400: CGFloat = 16
+  public static  let r500: CGFloat = 20
+  public static let rFull: CGFloat = 9999
+}
---
+++ Sources/DayDreamerDesignSystem/Spacing.swift
@@ -0,0 +1,19 @@
+//
+//  Spacing.swift
+//  DaydreamerDesignSystem
+//
+//  Created by Claudius Ma on 3/31/25.
+//
+import SwiftUI
+// Spacing
+public enum Spacing {
+  public static let s0: CGFloat = 0
+  public static let s50: CGFloat = 2
+  public static let s100: CGFloat = 4
+  public static let s150: CGFloat = 6
+  public static let s200: CGFloat = 8
+  public static let s300: CGFloat = 12
+  public static let s400: CGFloat = 16
+  public static let s600: CGFloat = 24
+  public static let s800: CGFloat = 32
+}
